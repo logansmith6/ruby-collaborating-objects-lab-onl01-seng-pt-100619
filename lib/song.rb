9fd6ad81fdfd06c919c7b7163a7802b6805bd679
Logan Smith <logsmith06@gmail.com>
2019-11-26 23:35:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,9 +23,9 @@ class Song
     splitter_artist = file.split(" - ")[0]
     splitter_song = file.split(" - ")[1]
     new_song = self.new(splitter_song)
-    new_song.artist.name = splitter_artist 
-    new_song 
-    
+    new_song.artist.name = splitter_artist
+    new_song
+
   end
 
 
