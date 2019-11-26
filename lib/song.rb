b03cf7ceec38c62001bb3b32445a39c0abdfe26c
Logan Smith <logsmith06@gmail.com>
2019-11-26 23:00:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,20 @@ require 'pry'
 class Song
 
   attr_accessor :name, :artist
+  @@all = []
 
   def initialize(name)
     @artist = self
     @name = name
+    save
   end
 
+  def save 
+    @@all << self 
+  end 
+
   def self.all
-    self
+    @@all 
   end
 
 
