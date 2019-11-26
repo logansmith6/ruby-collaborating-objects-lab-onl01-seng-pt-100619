2de0d98c73c17ed6aa20ade4072a8c6a737c7ed0
Logan Smith <logsmith06@gmail.com>
2019-11-26 23:35:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,13 +19,13 @@ class Song
     @@all
   end
 
-  def new_by_filename(file)
+  def self.new_by_filename(file)
     splitter_artist = file.split(" - ")[0]
     splitter_song = file.split(" - ")[1]
     new_song = self.new(splitter_song)
-    new_song.artist.name = splitter_artist
-    new_song
-
+    new_song.artist.name = splitter_artist 
+    new_song 
+    
   end
 
 
