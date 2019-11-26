f88d9a634388429d3effafe37f13cc151d023903
Logan Smith <logsmith06@gmail.com>
2019-11-26 23:45:03 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.chdir(@path)
+    filename = Dir.glob("*.mp3")
+    filename
+  end
+
+end 
