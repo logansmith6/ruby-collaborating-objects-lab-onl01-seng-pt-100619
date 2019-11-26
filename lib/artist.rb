6ad0686dd201783965e79d303fd6c93f943a8092
Logan Smith <logsmith06@gmail.com>
2019-11-26 23:05:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,4 +21,12 @@ class Artist
     song.artist = self
   end
 
+  def self.find_or_create_by_name(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else 
+      self.artist.name = name 
+    end 
+  end 
+
 end
